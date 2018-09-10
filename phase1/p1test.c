51932356fb973b3d8609378d63b8af9ecf7c662e
gstapes <stapletong@xavier.edu>
2018-09-10 16:18:37 -0400

    1123456789

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -260,7 +260,7 @@ void main() {
 	/* check removeBlocked and insertBlocked */
 	addokbuf("insertBlocked test #1 started  \n");
 	for (i = 10; i < MAXPROC; i++) {
-		addokbuf(i);
+		addokbuf("1");
 		procp[i] = allocPcb();
 		if (insertBlocked(&sem[i], procp[i]))
 			adderrbuf("insertBlocked(1): unexpected TRUE   ");

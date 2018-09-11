401911c8a07a695aeba4aa0b4537e5173be33500
chris <staudigelc@xavier.edu>
2018-09-11 13:53:03 -0400

    fuck your logging

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -267,6 +267,7 @@ void main() {
 	}
 	addokbuf("insertBlocked test #2 started  \n");
 	for (i = 0; i < 10; i++) {
+		addokbuf((char*)i);
 		procp[i] = allocPcb();
 		if (insertBlocked(&sem[i], procp[i]))
 			adderrbuf("insertBlocked(2): unexpected TRUE   ");

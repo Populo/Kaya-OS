27c46649fd9bed3e89208f89a2f8ef9e3f99a57a
Staudigel, Chris <staudigelc@xavier.edu>
2018-09-10 20:43:27 -0400

    idea

--- phase1/asl.c
+++ phase1/asl.c
@@ -132,7 +132,7 @@ int insertBlocked (int *semAdd, pcb_PTR p)
             addokbuf("b");
             new -> s_semAdd = semAdd;
             addokbuf("c");
-            insertProcQ(lol, p);
+            insertProcQ(new -> s_procQ, p);
             addokbuf("9");
         }
         else

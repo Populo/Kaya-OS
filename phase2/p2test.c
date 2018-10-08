75425fe96a9c029e35812730c144eaf76c78b594
gstapes <stapletong@xavier.edu>
2018-10-08 10:29:30 -0400

    p

--- phase2/p2test.c
+++ phase2/p2test.c
@@ -142,61 +142,6 @@ void print(char *msg) {
 	SYSCALL(VERHOGEN, (int)&term_mut, 0, 0);				/* V(term_mut) */
 }
 
-void addokbuf(char *strp) {
-	char *tstrp = strp;
-	while ((*mp++ = *strp++) != '\0')
-		;
-	mp--;
-	termprint(tstrp, 0);
-}
-
-unsigned int termprint(char * str, unsigned int term) {
-	memaddr * statusp;
-	memaddr * commandp;
-	devreg stat;
-	devreg cmd;
-	unsigned int error = FALSE;
-	
-	if (term < DEVPERINT) {
-		/* terminal is correct */
-		/* compute device register field addresses */
-		statusp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANSTATUS * DEVREGLEN));
-		commandp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANCOMMAND * DEVREGLEN));
-		
-		/* test device status */
-		stat = termstat(statusp);
-		if (stat == READY || stat == TRANSMITTED) {
-			/* device is available */
-			
-			/* print cycle */
-			while (*str != EOS && !error) {
-				cmd = (*str << CHAROFFSET) | PRINTCHR;
-				*commandp = cmd;
-
-				/* busy waiting */
-				stat = termstat(statusp);
-				while (stat == BUSY)
-					 stat = termstat(statusp);
-				
-				/* end of wait */
-				if (stat != TRANSMITTED)
-					error = TRUE;
-				else
-					/* move to next char */
-					str++;
-			} 
-		}
-		else
-			/* device is not available */
-			error = TRUE;
-	}
-	else
-		/* wrong terminal device number */
-		error = TRUE;
-
-	return (!error);		
-}
-
 
 /*                                                                   */
 /*                 p1 -- the root process                            */

88d50beaf26a4907a5ef92676733212efe44a4c9
gstapes <stapletong@xavier.edu>
2018-10-13 13:54:57 -0400

    wont do shit

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -291,7 +291,7 @@ void sysWaitIO(state_PTR old)
     else{
         currentProcess -> pcb_s.s_v0 = sem[index];
         debugC(4109);
-        LDST(&currentProcess -> pcb_s);
+        LDST(old);
     }
     debugC(4106);
 

e56c3652c38ca009f55034523155c3178d6957a0
chris <staudigelc@xavier.edu>
2018-10-28 16:46:29 -0500

    initial

--- phase2/initial.c
+++ phase2/initial.c
@@ -1,3 +1,16 @@
+/*****************************************************************
+ *                  initial.c                        
+ * initial.c contains the main function for the operating system. 
+ * This function initializes the pcb free list, ready queue, 
+ * and semaphore list. It initializes state locations for each 
+ * of the event handler locations: syscalls, pbgtraps, tlbtraps,
+ * and interrupts.  It creates a process, sets the pc to call 
+ * the p2test function right away, adds it to the readyQueue,
+ * and calls scheduler to kick off the operating system.
+ * 
+ * Writtern by Chris Staudigel and Grant Stapleton
+ *****************************************************************/
+
 #include "../h/const.h"
 #include "../h/types.h"
 
@@ -10,13 +23,23 @@
 
 #include "../e/initial.e"
 
+/************************Global Definitions***********************/
+
 int processCount;
 int softBlockCount;
 pcb_PTR currentProcess;
 pcb_PTR readyQueue;
 int sem[TOTALSEM];
 
-
+/***************************Main Function************************/
+/****************************************************************
+ * This function performs the work of the file by initializing
+ * the global variables, setting the appropriate states for the 
+ * handlers, initializing the pcb free list, ready Queue, 
+ * and ASL free list, creates a process to run p2test, and adds
+ * that process to the ready Queue, then calls scheduler to start
+ * the test.
+ *****************************************************************/
 int main()
 {
     unsigned int RAMTOP;
@@ -58,6 +81,7 @@ int main()
     processCount = 0;
     softBlockCount = 0;
     int temp;
+    /* initialize all device semaphores to 0 */
     for(temp = 0; temp < TOTALSEM; temp++)
     {
         sem[temp] = 0;
@@ -75,17 +99,21 @@ int main()
     currentProcess -> pcb_state.s_pc = (memaddr) test;
     /* i dont know what this is but we need to set this too */
     currentProcess -> pcb_state.s_t9 = (memaddr) test;
-    /* set the status */
+    /* set the status: All interrupts and local timer on */
     currentProcess -> pcb_state.s_status = ALLOFF | IEON | IMON | LTON;
 
+    /* increment process count */
     ++processCount;
 
+    /* insert new process to ready Queue */
     insertProcQ(&readyQueue, currentProcess);
     currentProcess = NULL;
     LDIT(INTTIME);
 
+    /* call scheduler to get the first process */
     scheduler();
 
+    /* should never reach this */
     return FAILURE;
 }
 

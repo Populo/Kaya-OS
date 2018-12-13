c11dfb0653e09877cf70a3acc808b8ea42551428
gstapes <stapletong@xavier.edu>
2018-12-13 14:44:36 -0500

    try this

--- h/const.h
+++ h/const.h
@@ -11,7 +11,7 @@
 #define PAGESIZE		4096	/* page size in bytes */
 #define WORDLEN			4		/* word size in bytes */
 #define PTEMAGICNO		0x2A
-#define MAXUSERPROC     1
+#define MAXUSERPROC     8
 #define SWAPSIZE        16  /* frames in swap */
 #define UPROCSTCKSIZE   2 * PAGESIZE
 
